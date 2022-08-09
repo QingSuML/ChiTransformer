aa4f79bf75a3f5b7ba02e6db28592d4942eb49a9
Coldflyer <sq0925@gmail.com>
2022-08-08 23:35:19 -0400

    updates

--- builder.py
+++ builder.py
@@ -411,11 +411,11 @@ def build(args):
         
         # Hyperparameter change due to different parameter initialization method used
         if args.dcr_mode in ["sp", "spectrum"]:
-            weight_dict = {"reprojection_loss": 1.0, "fp_loss" : 1.0,
-                           "orthog_reg": 1., "hoyer_reg": 0.1}
+            weight_dict = {"reprojection_loss": 1.0, "fp_loss" : 1e-3,
+                           "orthog_reg": 1., "hoyer_reg": 1e-4}
             losses = ["reprojection_loss", "fp_loss", "orthog_reg", "hoyer_reg"]
         else:
-            weight_dict = {"reprojection_loss": 1.0, "fp_loss" : 1.0}
+            weight_dict = {"reprojection_loss": 1.0, "fp_loss" : 1e-3}
             losses = ["reprojection_loss", "fp_loss"]
     else:
         raise NotImplementedError(f"{args.dataset} is not implemented.")
